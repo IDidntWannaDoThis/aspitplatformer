c76ff01688f07e96ccb221a85282a6c5479886ea
discooern <72135961+discooern@users.noreply.github.com>
2023-03-14 14:04:29 +0100

    Enemy

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -1,38 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(CapsuleCollider2D))]
-
-public class CharacterController2D : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
-    // Move player in 2D space
-    public float maxSpeed = 3.4f;
-    public float jumpHeight = 6.5f;
+    public Transform player;
     public float gravityScale = 1.5f;
-    public Camera mainCamera;
-
-    bool facingRight = true;
-    float moveDirection = 0;
-    bool isGrounded = false;
-    Vector3 cameraPos;
+    int speed = 1;
+    int MaxDist = 10;
+    bool isGrounded = true;
     Rigidbody2D r2d;
     CapsuleCollider2D mainCollider;
     Transform t;
+    bool facingRight = true;
+    float moveDirection = 0;
 
-    [SerializeField] private AudioClip[] playerSounds;
-
-    [SerializeField] private Animator _animator;
-    // _animator.SetInteger("AnimationState", Number);
-
-    [SerializeField] private GameObject audioManager;
-    private AudioScript audio;
-
-    // Use this for initialization
-    void Start()
+    private void Start()
     {
-        audio = audioManager.GetComponent<AudioScript>();
         t = transform;
         r2d = GetComponent<Rigidbody2D>();
         mainCollider = GetComponent<CapsuleCollider2D>();
@@ -42,33 +25,8 @@ public class CharacterController2D : MonoBehaviour
         facingRight = t.localScale.x > 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        // Movement controls
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
-        {
-            moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
-            if (isGrounded && r2d.velocity.y < 0.01f)
-            {
-                // Walking Animation
-                _animator.SetInteger("AnimationState", 1);
-            }
-        }
-        else
-        {
-            if (isGrounded || r2d.velocity.y < 0.01f)
-            {
-                moveDirection = 0;
-            }
-
-            // Idle Animation
-            if (isGrounded && r2d.velocity.y < 0.01f)
-            {
-                _animator.SetInteger("AnimationState", 0);
-            }
-        }
-
         // Change facing direction
         if (moveDirection != 0)
         {
@@ -84,22 +42,11 @@ public class CharacterController2D : MonoBehaviour
             }
         }
 
-        // Jump Animation
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Vector3.Distance(transform.position, player.position) <= MaxDist)
         {
-            _animator.SetInteger("AnimationState", 2);
-            audio.PlayAudio(playerSounds[0]);
+            transform.position += transform.forward * speed * Time.deltaTime;
         }
 
-        // Jumping
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
-        {
-            r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
-        }
-    }
-
-    void FixedUpdate()
-    {
         Bounds colliderBounds = mainCollider.bounds;
         float colliderRadius = mainCollider.size.x * 0.4f * Mathf.Abs(transform.localScale.x);
         Vector3 groundCheckPos = colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, colliderRadius * 0.9f, 0);
@@ -120,7 +67,7 @@ public class CharacterController2D : MonoBehaviour
         }
 
         // Apply movement velocity
-        r2d.velocity = new Vector2((moveDirection) * maxSpeed, r2d.velocity.y);
+        r2d.velocity = new Vector2((moveDirection) * speed, r2d.velocity.y);
 
         // Simple debug
         Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, colliderRadius, 0), isGrounded ? Color.green : Color.red);
